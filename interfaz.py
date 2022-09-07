0d98fffe632e42023383bcb6d75a41875666a461
elmerescandon <64042282+elmerescandon@users.noreply.github.com>
2022-09-07 14:43:45 -0500

    1st Sketch Finished

--- interfaz.py
+++ interfaz.py
@@ -1,105 +1,22 @@
 # -*- coding: utf-8 -*-
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *  
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from UI_design import *
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(803, 525)
-        MainWindow.setStyleSheet("")
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(290, 20, 271, 41))
-        self.textBrowser.setStyleSheet("background-color:transparent;")
-        self.textBrowser.setObjectName("textBrowser")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(450, 200, 50, 50))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_5.setGeometry(QtCore.QRect(450, 290, 50, 50))
-        self.pushButton_5.setObjectName("pushButton_5")
-        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_7.setGeometry(QtCore.QRect(300, 290, 50, 50))
-        self.pushButton_7.setObjectName("pushButton_7")
-        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_9.setGeometry(QtCore.QRect(300, 200, 50, 50))
-        self.pushButton_9.setObjectName("pushButton_9")
-        self.face2_button = QtWidgets.QPushButton(self.centralwidget)
-        self.face2_button.setGeometry(QtCore.QRect(690, 170, 61, 61))
-        self.face2_button.setObjectName("face2_button")
-        self.face1_button = QtWidgets.QPushButton(self.centralwidget)
-        self.face1_button.setGeometry(QtCore.QRect(690, 90, 61, 61))
-        self.face1_button.setObjectName("face1_button")
-        self.pushButton_14 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_14.setGeometry(QtCore.QRect(690, 250, 61, 61))
-        self.pushButton_14.setObjectName("pushButton_14")
-        self.pushButton_15 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_15.setGeometry(QtCore.QRect(690, 330, 61, 61))
-        self.pushButton_15.setObjectName("pushButton_15")
-        self.pushButton_16 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_16.setGeometry(QtCore.QRect(690, 400, 61, 61))
-        self.pushButton_16.setObjectName("pushButton_16")
-        self.pushButton_10 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_10.setGeometry(QtCore.QRect(370, 330, 50, 50))
-        self.pushButton_10.setObjectName("pushButton_10")
-        self.pushButton_11 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_11.setGeometry(QtCore.QRect(530, 240, 50, 50))
-        self.pushButton_11.setObjectName("pushButton_11")
-        self.pushButton_12 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_12.setGeometry(QtCore.QRect(380, 160, 50, 50))
-        self.pushButton_12.setObjectName("pushButton_12")
-        self.pushButton_13 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_13.setGeometry(QtCore.QRect(230, 250, 50, 50))
-        self.pushButton_13.setObjectName("pushButton_13")
-        self.pushButton_17 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_17.setGeometry(QtCore.QRect(50, 150, 111, 51))
-        self.pushButton_17.setObjectName("pushButton_17")
-        self.pushButton_18 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_18.setGeometry(QtCore.QRect(50, 210, 111, 51))
-        self.pushButton_18.setObjectName("pushButton_18")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 803, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+class UI_MainWindow(QMainWindow):
+    def __init__(self,*args,**kwargs):
+        super(UI_MainWindow,self).__init__(*args,**kwargs)
+        setupUi(self)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Jonas - El Robot</span></p></body></html>"))
-        self.pushButton_3.setText(_translate("MainWindow", "UR"))
-        self.pushButton_5.setText(_translate("MainWindow", "DR"))
-        self.pushButton_7.setText(_translate("MainWindow", "DL"))
-        self.pushButton_9.setText(_translate("MainWindow", "UL"))
-        self.face2_button.setText(_translate("MainWindow", "Cara 2"))
-        self.face1_button.setText(_translate("MainWindow", "Cara 1"))
-        self.pushButton_14.setText(_translate("MainWindow", "Cara 3"))
-        self.pushButton_15.setText(_translate("MainWindow", "Cara 4"))
-        self.pushButton_16.setText(_translate("MainWindow", "Cara 5"))
-        self.pushButton_10.setText(_translate("MainWindow", "DOWN"))
-        self.pushButton_11.setText(_translate("MainWindow", "LEFT"))
-        self.pushButton_12.setText(_translate("MainWindow", "UP"))
-        self.pushButton_13.setText(_translate("MainWindow", "RIGHT"))
-        self.pushButton_17.setText(_translate("MainWindow", "START"))
-        self.pushButton_18.setText(_translate("MainWindow", "STOP"))
-
+    # def retranslateUi(self, QMainWindow):
 
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    ui = UI_MainWindow()
+    ui.show()
     sys.exit(app.exec_())
